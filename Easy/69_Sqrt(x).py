167fcf21bad9ac1b666d6302ef2e993fec67648a
nazarovlex <collermix@gmail.com>
2023-03-15 22:45:48 +0300

    Problem not solved.

---
+++ Easy/69_Sqrt(x).py
@@ -0,0 +1,34 @@
+class Solution(object):
+    ## solution #1
+    # def mySqrt(self, x):
+    #     i = 0
+    #     while True:
+    #         if i*i==x or (i+1)*(i+1)>x:
+    #             return i
+    #         i += 1
+
+    ## solution #2
+    def mySqrt(self, x):
+        i = x // 2
+        c = 0
+        while True:
+            if x == 0:
+                return 0
+            elif 0 < x < 4:
+                return 1
+
+            if i * i > x:
+                c = i
+                i = i // 2
+
+            elif (i + 1) * (i + 1) < x:
+                i = i + (c - i) // 2
+            elif i * i == x or i * i < x and (i + 1) * (i + 1) > x:
+                return i
+            elif (i + 1) * (i + 1) == x:
+                return i + 1
+
+
+number = 2147395599
+result = Solution().mySqrt(number)
+print(result)
