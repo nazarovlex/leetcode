8ce0871ee9728fe0d53a752b3becd9d3eb6be2d2
nazarovlex <collermix@gmail.com>
2023-03-16 11:36:39 +0300

    Problem solved.

--- Easy/141_Linked_List_Cycle.py
+++ Easy/141_Linked_List_Cycle.py
@@ -1,29 +1,13 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
 class Solution(object):
     def hasCycle(self, head):
-        head2 = head
-        while head.next and head.next.next:
-            if head2 == head.next or head2 == head.next.next:
+        left = head
+        right = head
+
+        while right and right.next:
+            left = left.next
+            right = right.next.next
+            if left == right:
                 return True
-            head2 = head2.next
-            head = head.next.next
-        return False
 
-
-list_ar = [5, 7, 5]
-h = ListNode(val=1)
-cnt = 0
-while cnt < 5:
-    h = ListNode(val=list_ar[cnt % 2], next=h)
-    cnt += 1
-
-h = ListNode(val=1, next=h)
-
-result = Solution().hasCycle(h)
-print(result)
+        else:
+            return False
