a1c34dc16a1090af3cb7fa3258429a5bda4de1f7
nazarovlex <collermix@gmail.com>
2023-03-16 11:34:35 +0300

    Problem solved.

--- Easy/66_Plus_One.py
+++ Easy/66_Plus_One.py
@@ -12,12 +12,12 @@ class Solution(object):
 
     ## solution #2
     def plusOne(self, digits):
-        for i in range(len(digits)-1, -1, -1):
+        for i in range(len(digits) - 1, -1, -1):
             if digits[i] + 1 < 10:
                 digits[i] += 1
                 break
-            elif i==0:
-                digits.insert(0,1)
+            elif i == 0:
+                digits.insert(0, 1)
                 digits[1] = 0
             elif digits[i] == 9:
                 digits[i] = 0
