37a1a59062a6c395423c6ce87dd8b9560aa1823e
nazarovlex <collermix@gmail.com>
2023-03-20 13:48:49 +0300

    Problem solved.

--- Medium/7_Reverse_Integer.py
+++ Medium/7_Reverse_Integer.py
@@ -1,15 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
         x = str(x)[::-1]
-        while x[0]=="0" and len(x)>1:
+        while x[0] == "0" and len(x) > 1:
             x = x[1:]
-        if x[-1]=="-":
+        if x[-1] == "-":
             x = "-" + x[:-1]
         x = int(x)
-        if 2**31<x or   x<-2**31:
+        if 2 ** 31 < x or x < -2 ** 31:
             return 0
         else:
             return x
+
 
 number = -177
 result = Solution().reverse(number)
