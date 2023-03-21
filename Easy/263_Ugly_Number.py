0513650d63ff2b769d1f763387c0e964a427baaa
nazarovlex <collermix@gmail.com>
2023-03-21 14:50:38 +0300

    Problem solved.

---
+++ Easy/263_Ugly_Number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        elif n == 1:
+            return True
+        while n // 2 >= 2:
+            if n % 2 == 0:
+                n //= 2
+            elif n % 3 == 0:
+                n //= 3
+            elif n % 5 == 0:
+                n //= 5
+            else:
+                return False
+        return True
+
+
+number = 14  # False
+result = Solution().isUgly(number)
+print(result)
