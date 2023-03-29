deb0386dd06b8e7270910cb520d5099337c67c89
nazarovlex <collermix@gmail.com>
2023-03-29 13:47:24 +0300

    Problem solved.

---
+++ Medium/5. Longest Palindromic Substring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+
+        if len(s) == 1:
+            return s
+        for i in range(len(s)):
+            c = s.count(s[i])
+            j = s.rfind(s[i])
+            for _ in range(c):
+                if j > i:
+                    tmp = s[i:j + 1]
+                    if tmp == tmp[::-1] and len(res) < len(tmp):
+                        res = tmp
+                    else:
+                        j = s.rfind(s[i], 0, j)
+                else:
+                    if len(res) < len(s[i]):
+                        res = s[i]
+
+        return res
+
+
+s = "twbiqwtafgjbtolwprpdnymaatlbuacrmzzwhkpvuwdtyfjsbsqxrlxxtqkjlpkvpxmlajdmnyepsmczmmfdtjfbyybotpoebilayqzvqztqgddpcgpelwmriwmoeeilpetbxoyktizwcqeeivzgxacuotnlzutdowiudwuqnghjgoeyojikjhlmcsrctvnahnoapmkcrqmwixpbtirkasbyajenknuccojooxfwdeflmxoueasvuovcayisflogtpxtbvcxfmydjupwihnxlpuxxcclbhvutvvshcaikuedhyuksbwwjsnssizoedjkbybwndxpkwcdxaexagazztuiuxphxcedqstahmevkwlayktrubjypzpaiwexkwbxcrqhkpqevhxbyipkmlqmmmogrnexsztsbkvebjgybrolttvnidnntpgvsawgaobycfaaviljsvyuaanguhohsepbthgjyqkicyaxkytshqmtxhilcjxdpcbmvnpippdrpggyohwyswuydyrhczlxyyzregpvxyfwpzvmjuukswcgpenygmnfwdlryobeginxwqjhxtmbpnccwdaylhvtkgjpeyydkxcqarkwvrmwbxeetmhyoudfuuwxcviabkqyhrvxbjmqcqgjjepmalyppymatylhdrazxytixtwwqqqlrcusxyxzymrnryyernrxbgrphsioxrxhmxwzsytmhnosnrpwtphaunprdtbpwapgjjqcnykgspjsxslxztfsuflijbeebwyyowjzpsbjcdabxmxhtweppffglvhfloprfavduzbgkw"
+result = Solution().longestPalindrome(s)
+print(result)
