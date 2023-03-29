c72b448833a91c4b9fdbc3c4e81b76aaebb6f064
nazarovlex <collermix@gmail.com>
2023-03-29 08:49:31 +0300

    Problem solved.

---
+++ Medium/43_Multiply_Strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        return str(int(num1) * int(num2))
+
+
+num1 = "123"
+num2 = "456"
+result = Solution().multiply(num1, num2)
+print(result)
