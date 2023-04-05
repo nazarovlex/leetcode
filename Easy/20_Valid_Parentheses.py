0ff0dde141ff3f92a2a8c5e38526ddeeac3341a1
nazarovlex <collermix@gmail.com>
2023-04-05 13:10:29 +0300

    Optimized 1st solution and add 2nd solution using stack.

--- Easy/20_Valid_Parentheses.py
+++ Easy/20_Valid_Parentheses.py
@@ -1,32 +1,41 @@
-s = "([)]"    #False
-# s = "()()()"  #True
-# s = "([[((())]])"  #False
-# s = "{{{[[[()]]]}}(}" #False
-# s = "([](){})" #True
-# s = "{[]}"   #True
-# s = "[[[]"  # False
-print(s)
-
-
-def brackets_check(string):
-    opened_brackets = ["(", "[", "{"]
-    closed_brackets = [")", "]", "}"]
-    if len(string) % 2 > 0 or s[0] in closed_brackets or string[-1] in opened_brackets:
-        return False
-    i = 1
-    while i < len(string):
-        if string[i] in closed_brackets and string[i - 1] != opened_brackets[closed_brackets.index(string[i])]:
+class Solution(object):
+    def isValid(self, s):
+        opened_brackets = ["(", "[", "{"]
+        closed_brackets = [")", "]", "}"]
+        if len(s)%2>0 or s[0] in closed_brackets or s[-1] in opened_brackets:
             return False
+        i = 1
+        while i<len(s):
+            if s[i] in closed_brackets and s[i-1] != opened_brackets[closed_brackets.index(s[i])]:
+                return False
 
-        elif string[i] in closed_brackets and string[i - 1] == opened_brackets[closed_brackets.index(string[i])]:
-            string = string[:i - 1] + string[i + 1:]
-            i = 0
-            continue
-        elif i == len(string) - 1 and len(string) > 0:
-            return False
-        i += 1
-    return True
+            elif s[i] in closed_brackets and s[i-1] == opened_brackets[closed_brackets.index(s[i])]:
+                s = s[:i-1] + s[i+1:]
+                i = 0
+                continue
+            elif i == len(s)-1 and len(s)>0:
+                return False
+            i += 1
+        return True
 
 
-ret = brackets_check(s)
-print(ret)
+class Solution2:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2: return False
+        stack = []
+        pairs = {
+            "(": ")",
+            "[": "]",
+            "{": "}",
+        }
+        for bracket in s:
+            if bracket in pairs:
+                stack.append(bracket)
+            elif len(stack) == 0 or pairs[stack.pop()] != bracket:
+                return False
+        return len(stack) == 0
+
+
+s = "()[]{}"
+result = Solution().isValid(s)
+print(result)
