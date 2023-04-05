1191338226e03aa580fa4c916fadff6c603ed4f7
nazarovlex <collermix@gmail.com>
2023-04-05 11:33:50 +0300

    Problem solved.

---
+++ Easy/876. Middle of the Linked List.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        length = 0
+        nh = head
+        while nh:
+            length += 1
+            nh = nh.next
+        for _ in range(length // 2):
+            head = head.next
+        return head
+
+
+class Solution2:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        mas = []
+        while head:
+            mas.append(head.val)
+            head = head.next
+        head = None
+        print(mas)
+        for num in mas[len(mas) // 2:][::-1]:
+            head = ListNode(val=num, next=head)
+        return head
+
+
+head = ListNode(val=1, next=ListNode(val=2, next=ListNode(val=3, next=ListNode(val=4, next=ListNode(val=5)))))
+result = Solution().middleNode(head)
+while result:
+    print(result.val)
+    result = result.next
