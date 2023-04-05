4e26ce96295001b6144e2061c263d1163b409712
nazarovlex <collermix@gmail.com>
2023-04-05 12:54:30 +0300

    Add 2nd solution.

--- Easy/1_Two_Sum.py
+++ Easy/1_Two_Sum.py
@@ -1,13 +1,29 @@
+from typing import List
+
+
+# time: O(n*n) space: O(1)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+
+# Solution with extra space but xn faster
+# time: O(n) space: O(n)
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+        for i in range(len(nums)):
+            nums_dict[nums[i]] = i
+        for i in range(len(nums)):
+            cnt = target - nums[i]
+            if cnt in nums_dict.keys():
+                return [i, nums_dict[cnt]]
+
+
 nums = [2, 7, 11, 15]
 target = 9
-
-
-def two_sum(nums, target):
-    for i in range(len(nums)):
-        for j in range(len(nums)):
-            if nums[i] + nums[j] == target and i != j:
-                answer = [i, j]
-                return answer
-
-
-print(two_sum(nums, target))
+result = Solution().twoSum(nums, target)
+print(result)
