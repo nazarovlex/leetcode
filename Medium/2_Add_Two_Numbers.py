0bdcf5432d85119139741d47cf05cd20530650f0
nazarovlex <collermix@gmail.com>
2023-04-05 12:22:36 +0300

    Add 2nd solution.

--- Medium/2_Add_Two_Numbers.py
+++ Medium/2_Add_Two_Numbers.py
@@ -1,11 +1,14 @@
 # Definition for singly-linked list.
+from typing import Optional
+
+
 class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 
-class Solution(object):
+class Solution(object): # Brutal solution with array
     def addTwoNumbers(self, l1, l2):
         ln = None
         c = []
@@ -26,7 +29,7 @@ class Solution(object):
             return ListNode(val=c[0])
         elif len(c) == 1 and c[0] >= 10:
             c.append(c[-1] // 10)
-            c[-2] %= 10
+            c[0] %= 10
             return ListNode(val=c[0], next=ListNode(val=c[1], next=None))
         for i in range(1, len(c)):
             if c[i - 1] >= 10:
@@ -41,11 +44,25 @@ class Solution(object):
 
         return ln
 
+class Solution2: # Solution with cursor
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        curr = dummyHead
+        carry = 0
+        while l1 != None or l2 != None or carry != 0:
+            l1Val = l1.val if l1 else 0
+            l2Val = l2.val if l2 else 0
+            columnSum = l1Val + l2Val + carry
+            carry = columnSum // 10
+            newNode = ListNode(columnSum % 10)
+            curr.next = newNode
+            curr = newNode
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummyHead.next
 
-# list1 = ListNode(val=2, next=ListNode(val=4, next=ListNode(val=9, next=None)))
-# list2 = ListNode(val=5, next=ListNode(val=6, next=ListNode(val=4, next=ListNode(val=9, next=None))))
-# list1 = ListNode(val=2, next=ListNode(val=4, next=ListNode(val=3, next=None)))
-# list2 = ListNode(val=5, next=ListNode(val=6, next=ListNode(val=4, next=None)))
+
+
 list1 = ListNode(val=5, next=None)
 list2 = ListNode(val=5, next=None)
 
