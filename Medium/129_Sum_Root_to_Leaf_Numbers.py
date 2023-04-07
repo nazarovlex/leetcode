f9637d9a5a8c46c8c52e4f0c6490161e700b9213
nazarovlex <collermix@gmail.com>
2023-04-07 16:30:11 +0300

    Problem solved.

--- Medium/129_Sum_Root_to_Leaf_Numbers.py
+++ Medium/129_Sum_Root_to_Leaf_Numbers.py
@@ -1,44 +1,30 @@
-# Definition for a binary tree node.
-class TreeNode(object):
+from typing import Optional
+
+
+class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
 
-class Solution(object):
-    def sumNumbers(self, root):
-        def left(root, c):
-            while root:
-                c = c*10 + root.val
-                root = root.left
-            cnt.append(c)
-            c = 0
-        def right(root, c):
-            while root:
-                c = c*10 + root.val
-                root = root.right
-            cnt.append(c)
-            c = 0
-        def add_number(root,c):
-            while root:
-                print(cnt)
-                left(root,c)
-asdasd
-                add_number(root.left,c)
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        def helper(t, sum, res):
+            if not t:
+                return
+            sum = sum * 10 + t.val
+            if not t.right and not t.left:
+                res.append(sum)
+            helper(t.left, sum, res)
+            helper(t.right, sum, res)
+            sum -= t.val
 
-                # else:
-                #     add_number(root.left,c)
-                #     add_number(root.right,c)
-        cnt = []
-        c = 0
-        add_number(root,c)
-
-        print(cnt)
-        return sum(cnt)
+        res = []
+        helper(root, 0, res)
+        return sum(res)
 
 
-# tree = TreeNode(val=4, left=TreeNode(val=9, left=TreeNode(val=5), right=TreeNode(val=1)), right=TreeNode(val=0))
-tree = TreeNode(val=1, left=TreeNode(val=2), right=TreeNode(val=3))
+tree = TreeNode(val=1, left=TreeNode(val=2, left=TreeNode(val=3), right=TreeNode(val=2)))
 result = Solution().sumNumbers(tree)
 print(result)
