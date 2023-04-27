c8232eebae7ea7df064b93b6c5475ceba1282829
nazarovlex <collermix@gmail.com>
2023-04-27 12:46:00 +0300

    Problem solved.

--- Easy/338. Counting Bits.py
+++ Easy/338. Counting Bits.py
@@ -1,9 +1,10 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
         res = []
-        for i in range(n+1):
+        for i in range(n + 1):
             res.append(str(bin(i)[2:]).count("1"))
         return res
+
 
 n = 2
 result = Solution().countBits(n)
