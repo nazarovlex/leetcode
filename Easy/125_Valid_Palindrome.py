34bdc76cac3f539eb80f839f1bce5a5fabeba026
nazarov <collermix@gmail.com>
2023-05-22 16:59:04 +0300

    Add 2nd solution.

--- Easy/125_Valid_Palindrome.py
+++ Easy/125_Valid_Palindrome.py
@@ -13,6 +13,23 @@ class Solution(object):
             return False
 
 
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s) - 1
+        while i < j:
+            a, b = s[i].lower(), s[j].lower()
+            if a.isalnum() and b.isalnum():
+                if a != b:
+                    return False
+                else:
+                    i, j = i + 1, j - 1
+                    continue
+            i, j = i + (not a.isalnum()), j - (not b.isalnum())
+        return True
+
+
 string = "1A man, a plan, a canal: Panama1"
 result = Solution().isPalindrome(string)
+print(result)
+result = Solution2().isPalindrome(string)
 print(result)
