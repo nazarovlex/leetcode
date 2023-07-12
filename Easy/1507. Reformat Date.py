e23da4c01d081a0ca5b5d2c3d47d60c2ad3a8b4e
nazarov <collermix@gmail.com>
2023-07-12 16:52:14 +0300

    Problem solved.

---
+++ Easy/1507. Reformat Date.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reformatDate(self, date: str) -> str:
+        months_table = {
+            "Jan": "01",
+            "Feb": "02",
+            "Mar": "03",
+            "Apr": "04",
+            "May": "05",
+            "Jun": "06",
+            "Jul": "07",
+            "Aug": "08",
+            "Sep": "09",
+            "Oct": "10",
+            "Nov": "11",
+            "Dec": "12",
+        }
+        day, month, year = date.split(" ")
+        if len(day) == 3:
+            day = "0" + day
+        res = f"{year}-{months_table[month]}-{day[:-2]}"
+        return res
+
+
+date = "6th Jun 1933"
+result = Solution().reformatDate(date)
+print(result)
