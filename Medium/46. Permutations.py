d91b4179d649becfe81c763500fe859ad3b2fb57
nazarov <collermix@gmail.com>
2023-07-18 19:30:19 +0600

    Problem solved

---
+++ Medium/46. Permutations.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                ans.append(curr[:])
+                return
+
+            for num in nums:
+                if num not in curr:
+                    curr.append(num)
+                    backtrack(curr)
+                    curr.pop()
+
+        ans = []
+        backtrack([])
+        return ans
+
+
+nums = [5, 4, 6, 2]
+result = Solution().permute(nums)
+print(result)
