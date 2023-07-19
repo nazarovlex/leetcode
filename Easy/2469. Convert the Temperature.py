76574ebaeaa016decea0497d411b74f75d3a94d9
nazarov <collermix@gmail.com>
2023-07-19 20:23:46 +0600

    Problem solved

---
+++ Easy/2469. Convert the Temperature.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius + 273.15, celsius * 1.80 + 32.00]
+
+
+celsius = 36.50
+result = Solution().convertTemperature(celsius)
+print(result)
