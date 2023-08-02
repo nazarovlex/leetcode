30070d56d578843b5a25ea9029ae912cbbfe7dce
nazarov <collermix@gmail.com>
2023-08-02 15:56:46 +0600

    Problem solved

---
+++ Easy/1952. Three Divisors.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isThree(self, n: int) -> bool:
+        cnt = 0
+        for i in range(1, n + 1):
+            if n % i == 0:
+                cnt += 1
+        if cnt == 3:
+            return True
+        return False
+
+
+n = 4
+result = Solution().isThree(n)
+print(result)
