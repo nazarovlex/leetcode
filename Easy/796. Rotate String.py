b728fa0b9fcc1e39862d0ebaf88e2064c74969a2
nazarov <collermix@gmail.com>
2023-09-20 15:31:39 +0600

    Problem solved

---
+++ Easy/796. Rotate String.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        for _ in range(len(s)):
+            if s == goal:
+                return True
+            s = s[1:] + s[0]
+
+        return False
+
+
+s = "bbbacddceeb"
+goal = "ceebbbbacdd"
+result = Solution().rotateString(s, goal)
+print(result)
